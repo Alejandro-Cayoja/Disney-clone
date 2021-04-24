ea7e0da614a3d829ef44cf8f3c9c1ac99cd60d72
Alejandro Cayoja <Stronranger@gmail.com>
2021-04-24 12:34:08 -0300

    Agregando el boton de ingreso en el Navbar con algunos estilos

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,8 +32,9 @@ const Header = (props) => {
                     <span>SERIES</span>
                 </a>
             </NavMenu>
+            <Login>Login</Login>
         </Nav>
-    ) 
+    ); 
 };
 
 const Nav = styled.nav`
@@ -127,6 +128,21 @@ const NavMenu = styled.div`
 
     @media (max-width: 768px) {
         display: none;
+    }
+    `;
+
+const Login = styled.a`
+    background-color: rgba(0,0,0,0.6);
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    transition: all 0.2s ease 0s;
+
+    &:hover {
+        background-color: #f9f9f9;
+        
     }
     `;
 
