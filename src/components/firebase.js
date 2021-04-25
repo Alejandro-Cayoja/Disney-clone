f919eef13593792a8dfb87be570b74cb8a74bbd2
Alejandro Cayoja <Stronranger@gmail.com>
2021-04-25 12:09:02 -0300

    Detalles en la autenticacion de Firebase

--- src/components/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAH9FmLY1XEheG0Oxn9jgV3Pbvg-7totyA",
-    authDomain: "disneyplus-clone-ad7db.firebaseapp.com",
-    projectId: "disneyplus-clone-ad7db",
-    storageBucket: "disneyplus-clone-ad7db.appspot.com",
-    messagingSenderId: "691202593901",
-    appId: "1:691202593901:web:7ed6968230b47b9c081b64",
-    measurementId: "G-LEXKKMD5FE"
-  };
