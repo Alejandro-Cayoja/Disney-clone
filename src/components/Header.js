83f0c473c1ff0a91fc15c7569e04a7ba4d28f993
Alejandro Cayoja <Stronranger@gmail.com>
2021-04-27 11:57:26 -0300

    Arreglo de detalles en el codigo

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const Header = (props) => {
         auth.onAuthStateChanged(async (user) => {
             if(user) {
                 setUser(user)
-                history.push('/home')
+                history.push("/home")
             }
         })
     }, [userName]);
