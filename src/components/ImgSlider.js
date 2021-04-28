3839a03d0f087a0704ab43a76c27dfc32f517b45
Alejandro Cayoja <Stronranger@gmail.com>
2021-04-28 10:00:01 -0300

    Detalles esteticos del carusel implementados

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -13,8 +13,7 @@ const ImgSlider = (props) => {
         autoplay: true,
     };
     return (
-        <div>
-            <Slider {...settings}>
+            <Carousel {...settings}>
                 <div>
                     <h3>1</h3>
                 </div>
@@ -24,9 +23,47 @@ const ImgSlider = (props) => {
                 <div>
                     <h3>3</h3>
                 </div>
-            </Slider>
-        </div>
+            </Carousel>
     )
 };
+
+const Carousel = styled(Slider)`
+    margin-top: 20px;
+
+    & > button {
+        opacity: 0;
+        height: 100%;
+        width: 5vw;
+        z-index: 1;
+
+        &:hover {
+            opacity: 1;
+            transition: opacity 0.2s ease 0s;
+        }
+    }
+
+    ul li button {
+        &:before {
+            font-size: 10px;
+            color: rgb(150, 158, 171);
+        }
+    }
+
+    li.slick-active button:before {
+        color: white;
+    }
+
+    .slick-list {
+        overflow: initial;
+    }
+
+    .slick-prev {
+        left: -75px;
+    }
+
+    .slick-prev {
+        right: -75px;
+    }
+    `;
 
 export default ImgSlider;
